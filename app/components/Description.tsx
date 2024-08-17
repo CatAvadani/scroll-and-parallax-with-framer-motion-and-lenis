6164da23e185b6d80d4cbc5440b7a12183332ddf
CatAvadani <catalinaavadani@gmail.com>
2024-08-17 14:41:00 +0200

    Create first sections

---
+++ app/components/Description.tsx
@@ -0,0 +1,9 @@
+export default function Description() {
+  return (
+    <div className='flex justify-center my-40'>
+      <p className='text-[5.5vw] uppercase text-center max-w-[50vw] leading-none'>
+        A website animation with scroll and parallax effects.
+      </p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
+'use client';
+import Lenis from 'lenis';
+import { useEffect } from 'react';
+import Description from './components/Description';
+import Section from './components/Section';
+
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-4xl font-bold'>Animation Demo</h1>
-      <p className='text-lg'>
-        This is a basic project with website animation featuring a background
-        image moving on scroll in a parallax effect.
-      </p>
+    <main>
+      <Description />
+      <Section />
+      <div className=' h-screen'></div>
     </main>
   );
 }
