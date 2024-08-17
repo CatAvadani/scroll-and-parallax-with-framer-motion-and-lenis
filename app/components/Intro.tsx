0e0361261ab4027845e93cbd3eebaafe7236017a
CatAvadani <catalinaavadani@gmail.com>
2024-08-17 15:26:28 +0200

    Update the styles for intro

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -1,6 +1,7 @@
 import { motion, useScroll, useTransform } from 'framer-motion';
 import Image from 'next/image';
 import { useRef } from 'react';
+import IntroText from './IntroText';
 
 export default function Intro() {
   const container = useRef<HTMLDivElement>(null);
@@ -9,20 +10,25 @@ export default function Intro() {
     offset: ['start start', 'end start'],
   });
 
-  const y = useTransform(scrollYProgress, [0, 1], ['0vh', '150vh']);
+  const y = useTransform(scrollYProgress, [0, 1], ['-10vh', '10vh']);
 
   return (
-    <div className=' h-screen overflow-hidden'>
-      <motion.div style={{ y }} className='relative h-full w-full'>
-        <Image
-          src={
-            'https://images.pexels.com/photos/2049422/pexels-photo-2049422.jpeg?auto=compress&cs=tinysrgb&w=800'
-          }
-          fill
-          alt='image'
-          style={{ objectFit: 'cover' }}
-        />
-      </motion.div>
+    <div
+      ref={container}
+      className='relative flex items-center justify-center h-screen overflow-hidden'
+      style={{ clipPath: 'polygon(0% 0, 100% 0%, 100% 100%, 0 100%)' }}
+    >
+      <IntroText />
+      <div className='fixed top-[-10vh] left-0 h-[120vh] w-full'>
+        <motion.div style={{ y }} className='relative w-full h-full'>
+          <Image
+            src='https://images.pexels.com/photos/1710795/pexels-photo-1710795.jpeg?auto=compress&cs=tinysrgb&w=800'
+            alt='intro-img'
+            fill
+            className='object-cover'
+          />
+        </motion.div>
+      </div>
     </div>
   );
 }
